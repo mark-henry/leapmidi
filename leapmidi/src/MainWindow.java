093d28026c763253dd0c7a43854c33617d22c3da
Mark Henry <theratcheteer+github@gmail.com>
2013-04-09 11:54:16 -0700

    Created simple form in IntelliJ GUI designer

---
+++ leapmidi/src/MainWindow.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+/**
+ * MainWindow
+ */
+public class MainWindow
+{
+    private JPanel panel1;
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("MainWindow");
+        frame.setContentPane(new MainWindow().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
